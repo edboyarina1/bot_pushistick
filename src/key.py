f3f932acae89edd312b559c213e1de2d27c5dc89
edboyarina1 <you@example.com>
2025-04-28 21:29:19 +0300

    new names for buttons

--- src/key.py
+++ src/key.py
@@ -3,13 +3,13 @@ from telebot import types
 def get_admin_keyboard():
     """Клавиатура для администратора"""
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(types.KeyboardButton('/push'))
-    markup.add(types.KeyboardButton('/check'))
-    markup.add(types.KeyboardButton('/my_lessons'))
+    markup.add(types.KeyboardButton('push'))
+    markup.add(types.KeyboardButton('check'))
+    markup.add(types.KeyboardButton('мои занятия'))
     return markup
 
 def get_user_keyboard():
     """Клавиатура для  пользователя"""
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(types.KeyboardButton('/my_lessons'))
+    markup.add(types.KeyboardButton('мои занятия'))
     return markup
